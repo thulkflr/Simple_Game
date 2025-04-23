d4878a6fb813367ac240c7f30e15fd9ffc20cf8c
Thu Al-kfl Rababa'h <73723218+thulkflr@users.noreply.github.com>
2025-04-23 14:30:01 +0300

    win form

---
+++ thuylkflProject/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace thuylkflProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox1.Location = new System.Drawing.Point(73, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox2.Location = new System.Drawing.Point(555, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox3.Location = new System.Drawing.Point(424, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox4.Location = new System.Drawing.Point(317, 26);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox5.Location = new System.Drawing.Point(198, 26);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::thuylkflProject.Properties.Resources.Yellow_23;
+            this.pictureBox6.Location = new System.Drawing.Point(665, 26);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(57, 56);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = global::thuylkflProject.Properties.Resources.Rcket;
+            this.pictureBox7.Location = new System.Drawing.Point(354, 334);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(98, 95);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            this.pictureBox7.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.pictureBox7_PreviewKeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(39, 404);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "6";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.Image = global::thuylkflProject.Properties.Resources.A_perfect_SVG_heart;
+            this.pictureBox8.Location = new System.Drawing.Point(12, 398);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(32, 31);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(374, -2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "60";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(318, -2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Timer : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::thuylkflProject.Properties.Resources.istockphoto_628935254_170667a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bird Shooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BackgroundImageChanged += new System.EventHandler(this.Form1_Load);
+            this.BackgroundImageLayoutChanged += new System.EventHandler(this.Form1_BackgroundImageLayoutChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ thuylkflProject/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thuylkflProject
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+        int counter;
+        int timer;
+        int x;
+        int y;
+        int x1 ;
+        int y1 ;
+        int x2;
+        int y2;
+        int x3;
+        int y3;
+        int x4;
+        int y4;
+        int x5;
+        int y5;
+        int x6;
+        int y6;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            x = 354;
+            y = 334;
+            y1 = 26;
+            y2 = 26;
+            y3 = 26;
+            y4 = 26;
+            y5 = 26;
+            y6 = 26;
+            counter = 6;
+            timer = 60;
+
+            //x1 = 73;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           // label1.Text = e.Button.ToString();
+           // label1.Text = e.X +" , "+e.Y;
+           //label1.Location =  e.Location;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_BackgroundImageLayoutChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer -= 2;
+            label2.Text=timer.ToString();
+
+
+            pictureBox1.Location = new Point(73, y1 += 10);
+            pictureBox2.Location = new Point(555, y2 += 10);
+            pictureBox3.Location = new Point(424, y3 += 10);
+            pictureBox4.Location = new Point(317, y4 += 10);
+            pictureBox5.Location = new Point(198, y5 += 10);
+            pictureBox6.Location = new Point(665, y6 += 10);
+
+            Random randomInteger = new Random();
+            x1 = randomInteger.Next(26, 925);
+            pictureBox1.Left = x1;
+             Random randomInteger1 = new Random();
+            x2 = randomInteger.Next(555, 925);
+            pictureBox2.Left = x2;
+             Random randomInteger2 = new Random();
+            x3 = randomInteger.Next(424, 925);
+            pictureBox3.Left = x3;
+             Random randomInteger3 = new Random();
+            x4 = randomInteger.Next(317, 925);
+            pictureBox4.Left = x4;
+             Random randomInteger4 = new Random();
+            x5 = randomInteger.Next(198, 925);
+            pictureBox5.Left = x5;
+             Random randomInteger5 = new Random();
+            x6 = randomInteger.Next(665, 925);
+            pictureBox6.Left = x6;
+
+            if ((pictureBox7.Bounds.IntersectsWith(pictureBox1.Bounds) == true)|| (pictureBox7.Bounds.IntersectsWith(pictureBox2.Bounds) == true)|| (pictureBox7.Bounds.IntersectsWith(pictureBox3.Bounds) == true)|| (pictureBox7.Bounds.IntersectsWith(pictureBox4.Bounds) == true)|| (pictureBox7.Bounds.IntersectsWith(pictureBox5.Bounds) == true)|| (pictureBox7.Bounds.IntersectsWith(pictureBox6.Bounds) == true) )
+            {
+                label1.Text = counter.ToString();
+                counter--;
+                if (counter == 0)
+                {
+                    //   label1.Text = counter.ToString();
+                    MessageBox.Show("Game Over", "LOST", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                    timer1.Stop();
+
+                }
+               
+
+            }
+            if (timer == 0 && counter != 0)
+            {
+
+                MessageBox.Show("Winner Winner Checken dinner", "WIN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+                timer1.Stop();
+
+
+            }
+
+            //    timer1.Tick += new EventHandler(timer1_Tick);
+            //     for (counter = 0; counter <= 60; counter += 2)
+
+            //    timer1.Enabled = false;
+            //  MessageBox.Show("game Over", "Win", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+
+
+            // y1 = 73;
+            // pictureBox1.Image = new Point(x1, y1);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+         
+
+            if (e.KeyCode == Keys.Up)
+            {
+                y -= 10;
+              pictureBox7.Location = new Point(x,y );
+                //timer1.Enabled = true;
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                y += 10;
+                 pictureBox7.Location = new Point(x, y);
+                //timer1.Enabled = true;
+
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                x+=10;
+                pictureBox7.Location = new Point(x, y);
+              //  timer1.Enabled = true;
+
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                x -= 10;
+
+                pictureBox7.Location = new Point(x,  y);
+                timer1.Start();
+               
+              
+              
+
+            }
+
+        }
+
+        private void pictureBox7_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+          
+
+
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Move(object sender, EventArgs e)
+        {
+            //pictureBox1.Top = y1;
+
+            //Random randomInteger = new Random();
+            //x1 = randomInteger.Next(26, 925);
+            //pictureBox1.Left = x1;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
